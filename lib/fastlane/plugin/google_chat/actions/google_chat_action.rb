b7ea09ba360ea3c0d46f3ebc3c9556f48cd7dc36
papipons <alfonsedumadapat@gmail.com>
2024-06-07 06:50:20 +0800

    Update params

--- lib/fastlane/plugin/google_chat/actions/google_chat_action.rb
+++ lib/fastlane/plugin/google_chat/actions/google_chat_action.rb
@@ -9,40 +9,82 @@
       class GoogleChatAction < Action
         def self.run(params)
           uri = URI.parse(params[:webhook])
+          cardsV2 = {
+            "cardsV2": [
+              {
+                "card": {
+                  "header": {
+                    "title": params[:headerTitle],
+                    "subtitle": params[:headerSubtitle]
+                  },
+                  "sections": [
+                    {
+                      "widgets": [
+                      {
+                        "decoratedText": {
+                          "topLabel": "<font color=\"#4caf50\">#{params[:item1Title]}</font>",
+                          "text": params[:item1Subtitle]
+                        }
+                      },
+                      {
+                        "decoratedText": {
+                          "topLabel": "<font color=\"#4caf50\">#{params[:item2Title]}</font>",
+                          "text": params[:item2Subtitle]
+                        }
+                      },
+                      {
+                        "decoratedText": {
+                          "topLabel": "<font color=\"#4caf50\">#{params[:item3Title]}</font>",
+                          "text": params[:item3Subtitle]
+                        }
+                      },
+                      {
+                        "decoratedText": {
+                          "topLabel": "<font color=\"#4caf50\">#{params[:item4Title]}</font>",
+                          "text": params[:item4Subtitle]
+                        }
+                      },
+                      {
+                        "divider": {}
+                      },
+                      {
+                        "buttonList": {
+                          "buttons": [
+                            {
+                              "text": params[:buttonTitle],
+                              "color": {
+                                "red": 0.29,
+                                "green": 0.69,
+                                "blue": 0.31,
+                                "alpha": 1
+                              },
+                              "onClick": {
+                                "openLink": {
+                                  "url": params[:buttonUrl]
+                                }
+                              }
+                            }
+                          ]
+                        }
+                      }
+                    ]
+                    }
+                  ]
+                }
+              }
+            ]
+          }
 
           http = Net::HTTP.new(uri.host, uri.port)
           http.use_ssl = true
-          http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
-          bodyString = <<~HEREDOC
-          Distributed App
-          App Name: *New GlobeOne*
-          Platform: *#{params[:platform]}*
-          
-          Version: *#{params[:versionText]}*
-          CXS: *#{params[:cxsEnv]}*
-          Catalog: *#{params[:catalogEnv]}*
-          CMS: *#{params[:cmsVersionText]}*
-          Debug Menu: *#{params[:debugMenuText]}*
-          Git Branch: *#{params[:gitBranch]}*
-          
-          *Release Notes:*
-          -
-          
-          _#{params[:footerText]}_
-          HEREDOC
+          request = Net::HTTP::Post.new(uri.request_uri)
+          request["Content-Type"] = "application/json"
+          request.body = cardsV2.to_json
 
-          bodyText = {
-            text: bodyString
-          }
+          response = http.request(request)
 
-          # Chat Request
-          messageRequest = Net::HTTP::Post.new(uri.request_uri)
-          messageRequest["Content-Type"] = "application/json"
-          messageRequest.body = bodyText.to_json
-
-          # Send Chat Request
-          chatResponse = http.request(messageRequest)
+          UI.message("Message sent!")
         end
         
         def self.description
@@ -64,51 +106,99 @@
         
         def self.available_options
           [
-            FastlaneCore::ConfigItem.new(key: :webhook,
-                                    env_name: "GOOGLE_CHAT_webhook",
-                                 description: "Google chat space webhook",
-                                    optional: false,
-                                        type: String),
-            FastlaneCore::ConfigItem.new(key: :platform,
-                                    env_name: "GOOGLE_CHAT_platform",
-                                 description: "Platform",
-                                    optional: false,
-                                        type: String),
-            FastlaneCore::ConfigItem.new(key: :versionText,
-                                    env_name: "GOOGLE_CHAT_versionText",
-                                 description: "Version Text",
-                                    optional: false,
-                                        type: String),
-            FastlaneCore::ConfigItem.new(key: :cxsEnv,
-                                    env_name: "GOOGLE_CHAT_cxsEnv",
-                                 description: "CXS Environment",
-                                    optional: false,
-                                        type: String),
-            FastlaneCore::ConfigItem.new(key: :catalogEnv,
-                                    env_name: "GOOGLE_CHAT_catalogEnv",
-                                 description: "Catalog Environment",
-                                    optional: false,
-                                        type: String),
-            FastlaneCore::ConfigItem.new(key: :cmsVersionText,
-                                    env_name: "GOOGLE_CHAT_cmsVersionText",
-                                 description: "CMS Version Text",
-                                    optional: false,
-                                       type: String),
-            FastlaneCore::ConfigItem.new(key: :debugMenuText,
-                                    env_name: "GOOGLE_CHAT_debugMenuText",
-                                 description: "Debug Menu Text",
-                                    optional: false,
-                                        type: String),
-            FastlaneCore::ConfigItem.new(key: :footerText,
-                                    env_name: "GOOGLE_CHAT_footerText",
-                                 description: "Footer text",
-                                    optional: false,
-                                        type: String),
-            FastlaneCore::ConfigItem.new(key: :gitBranch,
-                                    env_name: "GOOGLE_CHAT_gitBranch",
-                                 description: "Git Branch",
-                                    optional: false,
-                                        type: String)
+            FastlaneCore::ConfigItem.new(
+              key: :webhook,    
+              env_name: "GOOGLE_CHAT_webhookl",
+              description: "Webhook URL",
+              optional: false,                          
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :headerTitle,                      
+              env_name: "GOOGLE_CHAT_headerTitle",
+              description: "Header title",
+              optional: false,
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :headerSubtitle,
+              env_name: "GOOGLE_CHAT_headerSubtitle",
+              description: "Header subtitle",
+              optional: false,
+              type: String
+            ),    
+
+            FastlaneCore::ConfigItem.new(
+              key: :item1Title,
+              env_name: "GOOGLE_CHAT_item1Title",
+              description: "Item 1 title",
+              optional: false,
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :item1Subtitle,
+              env_name: "GOOGLE_CHAT_item1Subtitle",
+              description: "Item 1 subtitle",
+              optional: false,
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :item2Title,
+              env_name: "GOOGLE_CHAT_item2Title",
+              description: "Item 2 title",
+              optional: false,
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :item2Subtitle,
+              env_name: "GOOGLE_CHAT_item2Subtitle",
+              description: "Item 2 subtitle",
+              optional: false,
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :item3Title,
+              env_name: "GOOGLE_CHAT_item3Title",
+              description: "Item 3 title",
+              optional: false,
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :item3Subtitle,
+              env_name: "GOOGLE_CHAT_item3Subtitle",
+              description: "Item 3 subtitle",
+              optional: false,
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :item4Title,
+              env_name: "GOOGLE_CHAT_item4Title",
+              description: "Item 4 title",
+              optional: false,
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :item4Subtitle,
+              env_name: "GOOGLE_CHAT_item4Subtitle",
+              description: "Item 4 subtitle",
+              optional: false,
+              type: String
+            ),
+
+            FastlaneCore::ConfigItem.new(
+              key: :buttonTitle,
+              env_name: "GOOGLE_CHAT_buttonTitle",
+              description: "Button title",
+              optional: false,
+              type: String
+            ),
+            FastlaneCore::ConfigItem.new(
+              key: :buttonUrl,
+              env_name: "GOOGLE_CHAT_buttonUrl",
+              description: "Button URL",
+              optional: false,
+              type: String
+            )
           ]
         end
         
